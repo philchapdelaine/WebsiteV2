66b630552a84b04dec6b124ec3db5016209ad7e9
philchapdelaine <pchapy@gmail.com>
2022-01-08 13:42:21 -0800

    basic styling done

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,17 +1,44 @@
 import React from 'react'
 import './Information.css'
+import Typical from 'react-typical'
 // import resume from '../images/philippe_chapdelaine_resume_2021.pdf'
 
 function Information() {
     
   return (
       <div className='InformationContainer'>
-        <div>Philippe Chapdelaine</div>
-        <div>
+        <div className='InformationName'>Philippe Chapdelaine</div>
+        <div className='InformationText'>
+            <Typical
+                steps={[
+                    '', 
+                    1000, 
+                    'Front End Developer.',
+                    2000,
+                    'Full Stack Developer.',
+                    2000,
+                    'Kind of a nerd.',
+                    2000
+                ]}
+                loop={Infinity}
+                wrapper="p"
+            />
+        </div>
+        <div className='InformationText'>pchapy@gmail.com</div>
+        <div className='InformationText'>Vancouver, BC</div>
+        <div className='InformationText'>604-356-1821</div>
+        <div className='InformationText'>Filter projects by type:</div>
+        <div className='InformationText'>
+            <span className='FrontEnd'>Front End Dev - </span>
+            <span className='FullStack'>Full Stack Dev - </span>
+            <span className='Algorithms'>Algorithms - </span>
+            <span className='UXResearch'>UX Research</span>
+        </div>
+        <div className='InformationText'>
             <p>
-                Philippe Chapdaine is a multi-disciplined developer designer currently leading teams at
-                <a href="https://hypergiant.com" target="_blank">Hypergiant</a>
-                and living in Elkhart, Indiana.
+                I'm Phil, a software developer, student, and occasional soccer player based in Vancouver.
+                Currently, I'm finshing a computer science degree at UBC. I'm seeking exciting opportunities in 2022.
+                Here are some of my projects.
             </p>
         </div>
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import './Projects.css'
 
+const projects = [
+  {
+    name: 'AE Proposal Builder',
+    year: 2021,
+    tech: 
+    [
+      'Front End Dev'
+    ]
+  },
+  {
+    name: 'Park Hangs',
+    year: 2020,
+    tech: 
+    [
+      'Full Stack Dev'
+    ]
+  },
+]
+
 function Projects() {
     
   return (
-      <div className='ProjectContainer'>I'll put projects here</div>
+      <div className='ProjectContainer'>
+        <ul className='ProjectList'>
+          {projects.map((project) => (
+            <div>
+              <li>
+                <span>{project.year}</span>
+                <span>{project.name}</span>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
   )
 }
 
