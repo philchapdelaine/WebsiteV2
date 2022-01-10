c02d56e20b3584f0678dbae79c231a038720bc8e
philchapdelaine <pchapy@gmail.com>
2022-01-09 19:55:54 -0800

    ADDED FADE IDK

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     setLoading(true);
     setTimeout(() => {
       setLoading(false)
-    }, 2000)
+    }, 0)
   }, [])
 
   return (
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react"
 import { useSelector, useDispatch } from "react-redux"
+import Fade from 'react-reveal/Fade'; 
 import './Projects.css'
 
 function Projects() {
@@ -8,20 +9,22 @@ function Projects() {
 
   return (
       <div className='ProjectContainer'>
-        <ul className='ProjectList'>
-          {projects.map((project, i) => (
-            <div>
-              <li id={i} className='ProjectElement'>
-                <span className='ProjectYear'>{project.year}</span>
-                <span>{project.name}</span>
-                {project.tech.map((technology) => 
-                  <span className='ProjectTech'>{technology}</span>
-                  )}
-                <div className='ProjectTagline'>{project.description}</div>
-              </li>
-            </div>
-          ))}
-        </ul>
+        <Fade top cascade>
+          <ul className='ProjectList'>
+            {projects.map((project, i) => (
+              <div>
+                <li id={i} className='ProjectElement'>
+                  <span className='ProjectYear'>{project.year}</span>
+                  <span>{project.name}</span>
+                  {project.tech.map((technology) => 
+                    <span className='ProjectTech'>{technology}</span>
+                    )}
+                  <div className='ProjectTagline'>{project.description}</div>
+                </li>
+              </div>
+            ))}
+          </ul>
+        </Fade>
       </div>
   )
 }
