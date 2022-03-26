69603d3b59bd44f4509838443e394dfec1268dc5
philchapdelaine <pchapy@gmail.com>
2022-03-26 16:47:58 -0700

    Update Information.js

--- src/components/Information.js
+++ src/components/Information.js
@@ -154,9 +154,9 @@ function Information() {
             </div>
             <div>
                 <div className='Experience'>Experience</div>
-                <div><span className='ExperienceYear'>2022-Pres</span>UBC Visual Cognition Lab</div>
-                <div><span className='ExperienceYear'>2019-20</span>Statistics Canada</div>
-                <div><span className='ExperienceYear'>2016-22</span>University of British Columbia</div>
+                <div><span className='ExperienceYear'>2022-Now</span>UBC Visual Cognition Lab</div>
+                <div><span className='ExperienceYear'>2019-20</span>   Statistics Canada</div>
+                <div><span className='ExperienceYear'>2016-22</span>   University of British Columbia</div>
             </div>
             {/*
             <div className='InformationText'>
